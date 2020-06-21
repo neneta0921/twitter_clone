b00cb726f84079e763bc51e353d0bfc2b5c16f0d
neneta0921 <info@neneta.net>
2020-06-21 10:13:49 +0900

    Google Analyticsタグを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,6 +87,18 @@
       @yield('content')
     </main>
   </div>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170142485-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-170142485-1');
+  </script>
 </body>
 
 </html>
